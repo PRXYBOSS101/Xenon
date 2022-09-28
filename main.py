0a9142bdcbe6e3a9ae5e9036c93ac9c9bd2a8f3d
pyro <90619153+pyroTM@users.noreply.github.com>
2022-09-27 21:28:46 -0500

    updating

--- main.py
+++ main.py
@@ -13,9 +13,24 @@ def main():
 
 @app.route(f'/{config["prefix"]}/<path:url>')
 def proxy(url):
-    r = requests.get(url)
-    html = r.text
-    
+    if url.startswith('http://') or url.startswith('https://'):
+        for i in config['blacklist']:
+            if i == url:
+                return "This site is blacklisted"
+            else:
+                continue
+        r = requests.get(url)
+        html = r.text
+    else:
+        url = f'http://{url}'
+        for i in config['blacklist']:
+            if i == url:
+                return "This site is blacklisted"
+            else:
+                continue
+        r = requests.get(url)
+        html = r.text
+
     return rewriter(html, config["prefix"], config["domain"], url)
 
 
